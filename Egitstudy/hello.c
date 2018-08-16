2f922b70329f27a19e98a7c1774311499d138df1
zg <zhaogang@bj.leadcoretech.com>
2018-08-16 14:17:58 +0800

    第一次发布

---
+++ Egitstudy/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+int main() {
+    printf("hello world!\n");
+    system("pause");
+    return 0;
+}
+/*
+ * hello.c
+ *
+ *  Created on: 2018Äê8ÔÂ15ÈÕ
+ *      Author: zhaogang
+ */
+
+
---
+++ Egitstudy/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main()
+{
+	std::cout<<"Hello World"<<std::endl;
+	std::cout<<"I am Wolangjushi\n";
+	std::cout<<"Please enter an integer:\n";
+	int a;
+	std::cin>>a;
+	if(a>1)
+		std::cout<<"Bigger than 1"<<std::endl;
+	else
+		std::cout<<"Smaller than 1"<<std::endl;
+	return 0;
+
+ /*
+ * main.cpp
+ *
+ *  Created on: 2018Äê8ÔÂ15ÈÕ
+ *      Author: zhaogang
+ */
+
+
